7c71f8e7444dea34be1673caaed2d1e703219c0a
Shangqian Gao <gao.shanqian@gmail.com>
2026-06-02 13:42:32 +0800

    Update convert_ckpt.py

--- convert_ckpt.py
+++ convert_ckpt.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 from safetensors.torch import load_file, save_file
 
-SRC_DIR = Path("/orange/sgao1/sgao1/continual_pretrain_outputs/tomoe_gated_llama3_8b/final")
+SRC_DIR = Path("/orange/sgao1/sgao1/continual_pretrain_outputs/tomoe_gated_llama3_8b/checkpoint-20000")
 DST_DIR = Path("/orange/sgao1/sgao1/continual_pretrain_outputs/tomoe_gated_llama3_8b/checkpoint-converted")
 
 
